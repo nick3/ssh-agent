a990a07ec42e9bb1a35a380919f04f02aba5db94
Nick Liu <imjustanemail@gmail.com>
2024-08-01 10:59:43 +0800

    feat: Add SSH_AUTH_SOCK socket binding on port 22222

--- run.sh
+++ run.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 case "$1" in
-    ssh-agent)
-        auth_dir=$(dirname "${SSH_AUTH_SOCK}")
-        env_dir=$(dirname "${auth_dir}")/env
-        rm -f "${SSH_AUTH_SOCK}"
-        mkdir -p "${auth_dir}"
-        chmod 700 "${auth_dir}"
+ssh-agent)
+	auth_dir=$(dirname "${SSH_AUTH_SOCK}")
+	env_dir=$(dirname "${auth_dir}")/env
+	rm -f "${SSH_AUTH_SOCK}"
+	mkdir -p "${auth_dir}"
+	chmod 700 "${auth_dir}"
 
-        # Create an envdir such that `chpst -e /ssh/env /bin/sh` works.
-        rm -rf "${env_dir}"
-        mkdir -p "${env_dir}"
-        echo "${SSH_AUTH_SOCK}" > "${env_dir}/SSH_AUTH_SOCK"
+	# Create an envdir such that `chpst -e /ssh/env /bin/sh` works.
+	rm -rf "${env_dir}"
+	mkdir -p "${env_dir}"
+	echo "${SSH_AUTH_SOCK}" >"${env_dir}/SSH_AUTH_SOCK"
 
-        exec /usr/bin/ssh-agent -a "${SSH_AUTH_SOCK}" -D
-        ;;
-    *)
-        exec $@;;
+	exec /usr/bin/ssh-agent -a "${SSH_AUTH_SOCK}" -D
+
+	socat TCP-LISTEN:22222,reuseaddr,fork UNIX-CONNECT:"${SSH_AUTH_SOCK}" &
+	;;
+*)
+	exec $@
+	;;
 esac
